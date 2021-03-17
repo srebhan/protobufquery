aa8d5886432c50040e663db689a7b8d96d7152ee
Sven Rebhan <sven.rebhan@googlemail.com>
2021-03-17 21:36:16 +0100

    Extend unit tests.

--- node_test.go
+++ node_test.go
@@ -8,20 +8,23 @@ import (
 )
 
 var addressbookSample = &addressbook.AddressBook{
-	People: []*addressbook.Person {
+	People: []*addressbook.Person{
 		{
 			Name:  "John Doe",
 			Id:    101,
 			Email: "john@example.com",
+			Age:   42,
 		},
 		{
 			Name: "Jane Doe",
 			Id:   102,
+			Age:  40,
 		},
 		{
 			Name:  "Jack Doe",
 			Id:    201,
 			Email: "jack@example.com",
+			Age:   12,
 			Phones: []*addressbook.Person_PhoneNumber{
 				{Number: "555-555-5555", Type: addressbook.Person_WORK},
 			},
@@ -30,6 +33,7 @@ var addressbookSample = &addressbook.AddressBook{
 			Name:  "Jack Buck",
 			Id:    301,
 			Email: "buck@example.com",
+			Age:   19,
 			Phones: []*addressbook.Person_PhoneNumber{
 				{Number: "555-555-0000", Type: addressbook.Person_HOME},
 				{Number: "555-555-0001", Type: addressbook.Person_MOBILE},
@@ -40,15 +44,15 @@ var addressbookSample = &addressbook.AddressBook{
 			Name:  "Janet Doe",
 			Id:    1001,
 			Email: "janet@example.com",
+			Age:   16,
 			Phones: []*addressbook.Person_PhoneNumber{
 				{Number: "555-777-0000"},
 				{Number: "555-777-0001", Type: addressbook.Person_HOME},
 			},
 		},
 	},
-	Tags: []string {"home", "private", "friends"},
+	Tags: []string{"home", "private", "friends"},
 }
-
 
 func TestParseAddressBookXML(t *testing.T) {
 	msg := addressbookSample.ProtoReflect()
@@ -57,6 +61,6 @@ func TestParseAddressBookXML(t *testing.T) {
 	require.Len(t, doc.ChildNodes(), 6)
 
 	xml := doc.OutputXML()
-	expected := `<?xml version="1.0"?><people><name>John Doe</name><id>101</id><email>john@example.com</email></people><people><name>Jane Doe</name><id>102</id></people><people><name>Jack Doe</name><id>201</id><email>jack@example.com</email><phones><number>555-555-5555</number><type>2</type></phones></people><people><name>Jack Buck</name><id>301</id><email>buck@example.com</email><phones><number>555-555-0000</number><type>1</type></phones><phones><number>555-555-0001</number></phones><phones><number>555-555-0002</number><type>2</type></phones></people><people><name>Janet Doe</name><id>1001</id><email>janet@example.com</email><phones><number>555-777-0000</number></phones><phones><number>555-777-0001</number><type>1</type></phones></people><tags><element>home</element><element>private</element><element>friends</element></tags>`
+	expected := `<?xml version="1.0"?><people><name>John Doe</name><id>101</id><email>john@example.com</email><age>42</age></people><people><name>Jane Doe</name><id>102</id><age>40</age></people><people><name>Jack Doe</name><id>201</id><email>jack@example.com</email><age>12</age><phones><number>555-555-5555</number><type>2</type></phones></people><people><name>Jack Buck</name><id>301</id><email>buck@example.com</email><age>19</age><phones><number>555-555-0000</number><type>1</type></phones><phones><number>555-555-0001</number></phones><phones><number>555-555-0002</number><type>2</type></phones></people><people><name>Janet Doe</name><id>1001</id><email>janet@example.com</email><age>16</age><phones><number>555-777-0000</number></phones><phones><number>555-777-0001</number><type>1</type></phones></people><tags><element>home</element><element>private</element><element>friends</element></tags>`
 	require.Equal(t, expected, xml)
 }
--- query_test.go
+++ query_test.go
@@ -1,6 +1,8 @@
 package protobufquery
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/antchfx/xpath"
@@ -9,8 +11,8 @@ import (
 )
 
 type keyValue struct {
-	key string
-	value string
+	key     string
+	value   string
 	missing bool
 }
 
@@ -41,15 +43,15 @@ func TestNavigator(t *testing.T) {
 		t.Fatal("node type is not RootNode")
 	}
 
-	expectedPeople := []keyValueList {
-		[]keyValue {
-			{key: "name",  value: "John Doe"},
-			{key: "id",    value: "101"},
+	expectedPeople := []keyValueList{
+		[]keyValue{
+			{key: "name", value: "John Doe"},
+			{key: "id", value: "101"},
 			{key: "email", value: "john@example.com"},
 		},
-		[]keyValue {
-			{key: "name",  value: "Jane Doe"},
-			{key: "id",    value: "102"},
+		[]keyValue{
+			{key: "name", value: "Jane Doe"},
+			{key: "id", value: "102"},
 			{key: "email", value: "", missing: true},
 		},
 	}
@@ -66,7 +68,7 @@ func TestNavigator(t *testing.T) {
 				require.Equal(t, v.value, nav.Value())
 			} else {
 				if i > 0 {
-					if ! nav.MoveToNext() {
+					if !nav.MoveToNext() {
 						// There is no other node so we passed the test
 						continue
 					}
@@ -85,7 +87,112 @@ func TestNavigator(t *testing.T) {
 	require.Equal(t, nav.Current().Type, DocumentNode, "expected 'DocumentNode'")
 }
 
-func TestQuery(t *testing.T) {
+func TestQueryNames(t *testing.T) {
+	msg := addressbookSample.ProtoReflect()
+	doc, err := Parse(msg)
+	require.NoError(t, err)
+
+	expected := []string{
+		"John Doe",
+		"Jane Doe",
+		"Jack Doe",
+		"Jack Buck",
+		"Janet Doe",
+	}
+
+	nodes, err := QueryAll(doc, "/descendant::*[name() = 'people']/name")
+	require.NoError(t, err)
+	require.Len(t, nodes, len(expected))
+	for i, name := range expected {
+		require.Equal(t, "name", nodes[i].Name)
+		require.EqualValues(t, name, nodes[i].Value())
+	}
+
+	nodes, err = QueryAll(doc, "//name")
+	require.NoError(t, err)
+	require.Len(t, nodes, len(expected))
+	for i, name := range expected {
+		require.Equal(t, "name", nodes[i].Name)
+		require.EqualValues(t, name, nodes[i].Value())
+	}
+
+	nodes, err = QueryAll(doc, "/people[3]/name")
+	require.NoError(t, err)
+	require.Len(t, nodes, 1)
+	require.Equal(t, "name", nodes[0].Name)
+	require.EqualValues(t, expected[2], nodes[0].Value())
+}
+
+func TestQueryPhoneNumberFirst(t *testing.T) {
+	msg := addressbookSample.ProtoReflect()
+	doc, err := Parse(msg)
+	require.NoError(t, err)
+
+	expected := []string{
+		"555-555-5555",
+		"555-555-0000",
+		"555-777-0000",
+	}
+	nodes, err := QueryAll(doc, "//phones[1]/number")
+	require.NoError(t, err)
+	require.Len(t, nodes, len(expected))
+	for i, name := range expected {
+		require.Equal(t, "number", nodes[i].Name)
+		require.EqualValues(t, name, nodes[i].Value())
+	}
+}
+
+func TestQueryPhoneNumberLast(t *testing.T) {
+	msg := addressbookSample.ProtoReflect()
+	doc, err := Parse(msg)
+	require.NoError(t, err)
+
+	expected := []string{
+		"555-555-5555",
+		"555-555-0002",
+		"555-777-0001",
+	}
+	nodes, err := QueryAll(doc, "//phones[last()]/number")
+	require.NoError(t, err)
+	require.Len(t, nodes, len(expected))
+	for i, name := range expected {
+		require.Equal(t, "number", nodes[i].Name)
+		require.EqualValues(t, name, nodes[i].Value())
+	}
+}
+
+func TestQueryPhoneNoEmail(t *testing.T) {
+	msg := addressbookSample.ProtoReflect()
+	doc, err := Parse(msg)
+	require.NoError(t, err)
+
+	nodes, err := QueryAll(doc, "/people[not(email)]/id")
+	require.NoError(t, err)
+	require.Len(t, nodes, 1)
+	require.Equal(t, "id", nodes[0].Name)
+	require.EqualValues(t, 102, nodes[0].Value())
+}
+
+func TestQueryPhoneAge(t *testing.T) {
+	msg := addressbookSample.ProtoReflect()
+	doc, err := Parse(msg)
+	require.NoError(t, err)
+
+	expected := []string{
+		"John Doe",
+		"Jane Doe",
+		"Jack Buck",
+	}
+	nodes, err := QueryAll(doc, "/people[age > 18]/name")
+	require.NoError(t, err)
+	require.Len(t, nodes, len(expected))
+	for i, name := range expected {
+		require.Equal(t, "name", nodes[i].Name)
+		require.EqualValues(t, name, nodes[i].Value())
+	}
+}
+
+func TestQueryJack(t *testing.T) {
 	msg := addressbookSample.ProtoReflect()
 	doc, err := Parse(msg)
 	require.NoError(t, err)
@@ -98,4 +205,32 @@ func TestQuery(t *testing.T) {
 	require.EqualValues(t, 201, nodes[0].Value())
 	require.Equal(t, "id", nodes[1].Name)
 	require.EqualValues(t, 301, nodes[1].Value())
+}
+
+func TestQueryExample(t *testing.T) {
+	msg := addressbookSample.ProtoReflect()
+	doc, err := Parse(msg)
+	require.NoError(t, err)
+
+	expected := []string{
+		"John Doe: ",
+		"Jane Doe: ",
+		"Jack Doe: 555-555-5555",
+		"Jack Buck: 555-555-0000,555-555-0001,555-555-0002",
+		"Janet Doe: 555-777-0000,555-777-0001",
+	}
+
+	nodes, err := QueryAll(doc, "//people")
+	require.NoError(t, err)
+	require.Len(t, nodes, len(expected))
+
+	for i, person := range nodes {
+		name := FindOne(person, "name").InnerText()
+		numbers := make([]string, 0)
+		for _, node := range Find(person, "phones/number") {
+			numbers = append(numbers, node.InnerText())
+		}
+		v := fmt.Sprintf("%s: %s", name, strings.Join(numbers, ","))
+		require.EqualValues(t, expected[i], v)
+	}
 }
